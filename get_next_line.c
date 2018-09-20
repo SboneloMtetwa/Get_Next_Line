94fd22c0c93c72049eb217947b5c2bd11201d156
Sibonelo MTETWA <smtetwa@e5r9p7.wethinkcode.co.za>
2018-09-20 10:46:52 +0200

    gnl

---
+++ get_next_line.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smtetwa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/08 07:44:54 by smtetwa           #+#    #+#             */
+/*   Updated: 2018/06/23 10:50:49 by smtetwa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int			ft_check_line(char **string, char **line, int fd)
+{
+	char			*temp;
+	int				length;
+
+	length = 0;
+	while (string[fd][length] != '\n' && string[fd][length] != '\0')
+		length++;
+	if (string[fd][length] == '\n')
+	{
+		*line = ft_strsub(string[fd], 0, length);
+		temp = ft_strdup(string[fd] + length + 1);
+		free(string[fd]);
+		string[fd] = temp;
+		if (string[fd][0] == '\0')
+			ft_strdel(&string[fd]);
+	}
+	else if (string[fd][length] == '\0')
+	{
+		if (length == 0)
+			return (0);
+		*line = ft_strdup(string[fd]);
+		ft_strdel(&string[fd]);
+	}
+	return (1);
+}
+
+int					get_next_line(const int fd, char **line)
+{
+	static char		*str[255];
+	char			buf[BUFF_SIZE + 1];
+	char			*temp;
+	int				result;
+
+	if (fd < 0 || line == NULL)
+		return (-1);
+	while ((result = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[result] = '\0';
+		if (str[fd] == NULL)
+			str[fd] = ft_strnew(0);
+		temp = ft_strjoin(str[fd], buf);
+		free(str[fd]);
+		str[fd] = temp;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if (result < 0)
+		return (-1);
+	else if (result == 0 && (str[fd] == NULL || str[fd][0] == '\0'))
+		return (0);
+	return (ft_check_line(str, line, fd));
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smtetwa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/23 08:51:49 by smtetwa           #+#    #+#             */
+/*   Updated: 2018/06/23 11:40:11 by smtetwa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include "libft/libft.h"
+# include <fcntl.h>
+# define BUFF_SIZE 1
+
+int					get_next_line(const int fd, char **line);
+
+#endif
